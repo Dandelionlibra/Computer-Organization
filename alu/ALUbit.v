4708a25b83e8a515dbc96eb639dfeed6c77d7d19
ja-errorpro <compileerr0r@ja-errorpro.codes>
2024-05-07 16:14:52 +0800

    Add ALUbit and FullAdder module

---
+++ alu/ALUbit.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ns
+
+module ALUbit( a, b,bitInvert,cin,less, operation, dataOut, set, cout );
+// 1 bit ALU
+input a, b, bitInvert, cin, less ;
+input [5:0] operation ;
+output dataOut, set, cout;
+
+wire cin;
+wire andOut, orOut, xorOut;
+
+and(andOut, a, b);
+or(orOut, a, b);
+xor(xorOut, bitInvert, b);
+
+FullAdder U_FA( .a(a), .b(xorOut), .cin(cin), .sum(set), .cout(cout) );
+
+Mux4_1 U_Mux4_1( .out(dataOut), .in0(andOut), .in1(orOut), .in2(set), .in3(less), .sel(operation) );
+
+endmodule
---
+++ alu/FullAdder.v
@@ -0,0 +1,10 @@
+`timescale 1ns/1ns
+
+module FullAdder( a, b, cin, sum, cout );
+input a, b, cin;
+output sum, cout;
+
+assign sum = a ^ b ^ cin;
+assign cout = (a & b) | ((a ^ b) & cin);
+
+endmodule
